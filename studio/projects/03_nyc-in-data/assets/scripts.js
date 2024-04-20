5a3bac3d9f4d1f149abb38774ef6cbec108a4261
marrrrk03 <wongm208@newschool.edu>
2024-04-20 14:50:08 -0400

    commit 58
    alt text + SWC workshops and js for project 3

--- studio/projects/03_nyc-in-data/assets/scripts.js
+++ studio/projects/03_nyc-in-data/assets/scripts.js
@@ -1,14 +1,41 @@
 var dateObject = new Date();
-var year = dateObject.getFullYear();
-var month = String(dateObject.getMonth()).padStart(2, '0');
-var day = String(dateObject.getDay()).padStart(2, '0');
-var URL = `https://api.open-meteo.com/v1/forecast?latitude=40&longitude=75&current=relative_humidity_2m&timezone=America%2FNew_York`;
+var URL = `https://api.open-meteo.com/v1/forecast?latitude=40.7143&longitude=-74.006&current=temperature_2m,relative_humidity_2m&timezone=America%2FNew_York`;
 
 fetch(URL)
-	.then((response) => {
-		return response.json();
-	})
-	.then((data) => {
-		console.log(data);
-	})
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+        
+        var currentTemp = data.current.temperature_2m;
+        
+        document.getElementById('feet').innerText = `The temperature right now is: ${currentTemp}°C`;
+    })
 
+$(document).ready(function() {
+    
+    // var emojis = ['🪨'];
+
+    makeDiv();
+
+    function makeDiv() {
+        var numRand = Math.floor(Math.random() * 501);
+        var divsize = 100;
+        var posx = Math.random() * ($(window).width() - divsize);
+        var posy = Math.random() * ($(window).height() - divsize);
+        
+        var randomIndex = Math.floor(Math.random() * rocks.length);
+        var rock = rocks[randomIndex];
+
+        var $newdiv = $("<img src='assets/rockPNGs/" + rock + "' class='tree'></div>").css({
+            'left': posx + 'px',
+            'top': posy + 'px',
+			'filter': 'blur(1px)'
+        });
+
+        $newdiv.appendTo('body').delay(1500).fadeIn(0, function() {
+            makeDiv();
+        });
+    }
+});
