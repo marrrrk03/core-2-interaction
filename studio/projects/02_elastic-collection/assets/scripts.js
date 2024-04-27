a20231e613a0ab757cc0a503d90d97db3cf47463
marrrrk03 <wongm208@newschool.edu>
2024-04-27 15:13:20 -0400

    commit 68
    documenting p2

--- studio/projects/02_elastic-collection/assets/scripts.js
+++ studio/projects/02_elastic-collection/assets/scripts.js
@@ -1,24 +1,24 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Get the full-viewport div
     var cover = document.getElementById("cover");
 
     // Add event listener for click event
-    cover.addEventListener("click", function() {
+    cover.addEventListener("click", function () {
         // Hide the full-viewport div
         cover.style.display = "none";
     });
 });
 
 //tap to play audio
-document.addEventListener("click", function() {
-    document.getElementById('music').play();
+document.addEventListener("click", function () {
+    document.getElementById("music").play();
 });
 
 // find conatiner element in html
-var containerElement = document.querySelector('.treeContainer');
+var containerElement = document.querySelector(".treeContainer");
 
-
-function shuffleArray(array) {			//to randomize array
+function shuffleArray(array) {
+    //to randomize array
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
@@ -29,29 +29,25 @@ function shuffleArray(array) {			//to randomize array
 // shuffle the captions array
 shuffleArray(captions);
 
-
 // index to keep track of previous displayed captions
 let currentIndex = 0;
 
-
 function insertTreeSlide() {
-	const nextCaption = captions[currentIndex];
-	currentIndex = (currentIndex + 1) % captions.length; // move to the next caption or restart
-	const randomImageIndex = Math.floor(Math.random() * images.length);
+    const nextCaption = captions[currentIndex];
+    currentIndex = (currentIndex + 1) % captions.length; // move to the next caption or restart
+    const randomImageIndex = Math.floor(Math.random() * images.length);
     const randomImage = images[randomImageIndex];
-	const slide = document.createElement('div');
-	slide.classList.add('treeSlide');
-	slide.innerHTML = `
-		<img src="assets/media/${ randomImage }" class="tree" alt="this is a tree">
-		<p><span class="brackets">[</span>${ nextCaption }<span class="brackets">]</span></p>
+    const slide = document.createElement("div");
+    slide.classList.add("treeSlide");
+    slide.innerHTML = `
+		<img src="assets/media/${randomImage}" class="tree" alt="this is a tree">
+		<p><span class="brackets">[</span>${nextCaption}<span class="brackets">]</span></p>
 =	`;
     containerElement.appendChild(slide);
 }
 
-
 // initial insertion
 insertTreeSlide();
 
-
 // calling function to happen at interval
-setInterval(insertTreeSlide, 7500);
+setInterval(insertTreeSlide, 1500);
